54304cb727cf9c2313be2d7b3e1953344addf90e
gitCauchy <mailCauchy@gmail.com>
2018-11-28 15:12:54 +0800

    I have finished all the code of generate SqlMap

---
+++ src/com/cauchy/mybatis/po/Item.java
@@ -0,0 +1,65 @@
+package com.cauchy.mybatis.po;
+
+import java.util.Date;
+
+public class Item {
+    private Integer id;
+
+    private String name;
+
+    private Float price;
+
+    private String pic;
+
+    private Date creattime;
+
+    private String detail;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getPic() {
+        return pic;
+    }
+
+    public void setPic(String pic) {
+        this.pic = pic;
+    }
+
+    public Date getCreattime() {
+        return creattime;
+    }
+
+    public void setCreattime(Date creattime) {
+        this.creattime = creattime;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+}
